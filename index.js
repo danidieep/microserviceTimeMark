be58c8b682bc0ece61072c75b446eeed983a50ed
Daniel Dip <dani.dip.2@gmail.com>
2023-01-12 17:55:34 -0300

    arreglo deploy

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const PORT = process.env.PORT || 3000
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-const { now } = require('mongoose');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
